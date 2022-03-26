223cf6fe7848c65f8554ba041e763b4c9984bcaa
박지훈 <jihun.park8@cj.net>
2022-03-27 04:36:05 +0900

    220327-item

---
+++ src/main/java/com/jpabook/jpashop/controller/BookForm.java
@@ -0,0 +1,29 @@
+package com.jpabook.jpashop.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * packageName    : com.jpabook.jpashop.controller
+ * fileName       : BookForm
+ * author         : Jihun Park
+ * date           : 2022/03/27
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022/03/27        Jihun Park       최초 생성
+ */
+@Getter @Setter
+public class BookForm {
+
+    private Long id;
+    //item
+    private String name;
+    private int price;
+    private int stockQuantity;
+    //book
+    private String author;
+    private String isbn;
+
+}
---
+++ src/main/java/com/jpabook/jpashop/controller/ItemController.java
@@ -0,0 +1,46 @@
+package com.jpabook.jpashop.controller;
+
+import com.jpabook.jpashop.domain.item.Book;
+import com.jpabook.jpashop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+/**
+ * packageName    : com.jpabook.jpashop.controller
+ * fileName       : ItemController
+ * author         : Jihun Park
+ * date           : 2022/03/27
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022/03/27        Jihun Park       최초 생성
+ */
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createForm(Model model){
+        model.addAttribute("form", new BookForm());
+        return "items/creatItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public String create(BookForm bookForm){
+        Book book = new Book(); // 원래 setter는 다 제거하는게 더 좋음!
+        book.setName(bookForm.getName());
+        book.setPrice(bookForm.getPrice());
+        book.setStockQuantity(bookForm.getStockQuantity());
+        book.setAuthor(bookForm.getAuthor());
+        book.setIsbn(bookForm.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/";
+    }
+}
